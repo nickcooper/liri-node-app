8d9692f590621b13d3b0645d03cc33f15d642d47
Nick Cooper <charliecoop@gmail.com>
2019-10-02 21:53:13 -0600

    Cleaned up code, added print to log file, updated README

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# liri-node-app
+# LIRI Bot 
+(Language Interpretation and Recognition Interface)
+
+## Purpose
+LIRI is a command line app that allows you to search infomation on concerts, songs, and movies
+
+## Overview
+LIRI uses [Bands in Town](https://www.bandsintown.com/) for concert info, [Spotify](https://www.spotify.com) for song info, and [OMDB](https://www.omdbapi.com) for movie info.
+
+## How to use
+
+### Concert this
+Use the following format:
+```sh
+$ node liri.js concert-this <band-name>
+```
+![Concert this](images/concert.gif)
+
+### Spotify this
+Use the following format:
+```sh
+$ node liri.js spotify-this-song <song-name>
+```
+![Spotify this](images/spotify.gif)
+
+### Movie this
+Use the following format:
+```sh
+$ node liri.js movie-this <movie-name>
+```
+![Movie this](images/movie.gif)
+
+### Do what it says
+Reads the local file random.txt and parses the command from it.
+
+### Technologies Used
+* npm node modules:
+    * axios
+    * dotenv
+    * moment
+* APIs:
+    * node-spotify-api
+    * bands in town rest api
+    * omdb api
--- liri.js
+++ liri.js
@@ -1,30 +1,39 @@
+// includes
 var fs = require("fs");
 var axios = require("axios");
 var dotenv = require("dotenv").config();
 var moment = require("moment");
 
-var Spotify = require("node-spotify-api");
-
 var keys = require("./keys.js");
-
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-console.log(process.argv);
-
+// parse command line arguements
 var action = process.argv[2];
-var query = process.argv[3];
+var query = process.argv.slice(3).join(" ");
 
+// Log to file helper function
+function logIt(data) {
+    fs.appendFile("./log.txt", data, function (err) {
+        if (err) { throw err }
+    })
+}
 
+// get concert info function
 function _concert(_query) {
+    if (!_query) {
+        _query = "Celine Dion";
+    }
     axios.get(`https://rest.bandsintown.com/artists/${_query}/events?app_id=codingbootcamp`)
         .then(function (res) {
+            var output = `############\nConcert results for: ${_query}\n`;
             res.data.forEach(function (item) {
-                console.log(`Venue: ${item.venue.name}`);
-                console.log(`Location: ${item.venue.city} ${item.venue.region}`);
-                var formatted = moment(item.datetime).format("dddd, MMMM Do YYYY, h:mm:ss a");
-                //console.log(item.datetime);
-                console.log(`Date and Time: ${formatted}\n`);
+                var formatted = moment(item.datetime).format("MM/DD/YYYY");
+                output += `Venue: ${item.venue.name}\nLocation: ${item.venue.city} ${item.venue.region}\nDate and Time: ${formatted}\n\n`;
             });
+
+            console.log(output);
+            logIt(output);
 
         })
         .catch(function (err) {
@@ -32,56 +41,59 @@ function _concert(_query) {
         });
 }
 
+// get spotify info function
 function _spotify(_query) {
+    if (!_query) {
+        _query = "The Sign";
+    }
     spotify.search({ type: 'track', query: _query })
         .then(function (res) {
-            //console.log(response);
+            var output = `############\nSong results for: ${_query}\n`;
             res.tracks.items.forEach(function (item) {
-                console.log(`Artist: ${item.artists[0].name}`);
-                console.log(`Song name: ${item.name}`);
-                console.log(`Preview link: ${item.external_urls.spotify}`);
-                console.log(`Album: ${item.album.name}\n`);
+                output += `Artist: ${item.artists[0].name}\nSong name: ${item.name}\nPreview link: ${item.external_urls.spotify}\nAlbum: ${item.album.name}\n\n`;
             });
+
+            console.log(output);
+            logIt(output);
         })
         .catch(function (err) {
             console.log(err);
         });
 }
 
+// get movie info function
 function _movie(_query) {
+    if (!_query) {
+        _query = "Mr. Nobody";
+    }
     axios.get(`http://www.omdbapi.com/?t=${_query}&y=&plot=short&apikey=trilogy`)
         .then(function (res) {
-            //console.log(res.data);
-            console.log(`Title: ${res.data.Title}`);
-            console.log(`Year: ${res.data.Year}`);
-            console.log(`IMDB Rating: ${res.data.imdbRating}`);
-            console.log(`Rotten Tomatoes Rating: ${res.data.Ratings[1].Value}`);
-            console.log(`Country Produced: ${res.data.Country}`);
-            console.log(`Language: ${res.data.Language}`);
-            console.log(`Plot: ${res.data.Plot}`);
-            console.log(`Actors: ${res.data.Actors}\n`);
+            var output = `############\nMovie results for: ${_query}\n`;
+            output += `Title: ${res.data.Title}\nYear: ${res.data.Year}\nIMDB Rating: ${res.data.imdbRating}\nRotten Tomatoes Rating: ${res.data.Ratings[1].Value}\nCountry Produced: ${res.data.Country}\nLanguage: ${res.data.Language}\nPlot: ${res.data.Plot}\nActors: ${res.data.Actors}\n\n`;
+
+            console.log(output);
+            logIt(output);
         })
         .catch(function (error) {
             console.log(error);
         });
 }
 
+// read from file and to it function
 function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             return console.log(error);
         }
-        //console.log(data);
-
         var dataArr = data.split(",");
-console.log(dataArr);
         // We will then re-display the content as an array for later use.
-        route(dataArr[0], dataArr[1]);
+        routeMe(dataArr[0], dataArr[1]);
 
     });
 }
 
-function route(_action, _query) {
+// router function
+function routeMe(_action, _query) {
     switch (_action) {
         case "concert-this":
             _concert(_query);
@@ -100,4 +112,4 @@ function route(_action, _query) {
     }
 }
 
-route(action, query);
+routeMe(action, query);
